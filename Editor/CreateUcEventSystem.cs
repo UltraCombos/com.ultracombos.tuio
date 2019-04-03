ed986c75668e7c20a6a2da8a961d0db268f5a98d
other place <tech@ultracombos.com>
2019-04-03 19:27:12 +0800

    fix menu item

--- Editor/CreateUcEventSystem.cs
+++ Editor/CreateUcEventSystem.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 using UnityEditor;
 public static class CreateUCEventSystem
 {
-    [MenuItem("GameObject/UC/UC Event System",false , 0)]
+    [MenuItem("GameObject/Ultra Combos/UC Event System",false , 0)]
     private static void CreateUceventSystem()
     {
-        GameObject temp = AssetDatabase.LoadAssetAtPath("Assets/UC/TUIO/UCEventSystem.prefab", typeof(GameObject)) as GameObject;
+        //GameObject temp = AssetDatabase.LoadAssetAtPath("Assets/UC/TUIO/UCEventSystem.prefab", typeof(GameObject)) as GameObject;
+        GameObject temp = AssetDatabase.LoadAssetAtPath("Packages/com.ultracombos.tuio/Runtime/UCEventSystem.prefab", typeof(GameObject)) as GameObject;
 
         if (PrefabUtility.GetPrefabType(temp) == PrefabType.Prefab || PrefabUtility.GetPrefabType(temp) == PrefabType.ModelPrefab)
             PrefabUtility.InstantiatePrefab(temp);
